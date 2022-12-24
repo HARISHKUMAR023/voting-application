043447903c2dbf893bcd74ad455f4f8a4d01d23e
Harishkumar <harishkumarsp023@gmail.com>
2022-12-24 21:51:33 +0530

    the  backend code is updated

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const SHA256 = require("crypto-js/sha256");
+const bodyParser = require("Body-Parser");
+
 
 // serving the file
 const path = require('path');
@@ -18,18 +20,55 @@ app.use(express.urlencoded({ extended: false }));
 //html
 app.get('/', function (req, res) {
     const htmlfilepath = path.join(__dirname, 'views', 'index.html')
-    res.sendfile(htmlfilepath)
+    res.sendFile(htmlfilepath)
     //res.render('confirm');
 })
 app.get('/voter', function (req, res) {
+  
     const htmlfilepath = path.join(__dirname, 'views', 'pages/voting.html')
-    res.sendfile(htmlfilepath)
+    res.sendFile(htmlfilepath)
     //res.render('confirm');
+    app.post("/voter", function (req,res){
+        const divya = [1,1];
+        const karthi =[1,1];
+        const harish =[];
+        const akshaya =[];
+        const vote1 = req.body.nominee;
+        const name = req.body.name;
+        const voterid = req.body.voterid;
+        const composed = "voterName : "+ name + ", voter ID: " + voterid + ", Voting nominee : " + vote1;
+        console.log(composed);
+
+        if (vote1 == 1){
+            const push1 = divya.push(1);
+            const voter1 = divya.length;
+            console.log("total votes for Akshaya " + voter1);
+        }else if(vote1 == 2){
+            const push2 = karthi.push(1);
+            const voter2 = karthi.length;
+            console.log("Total vote for Karthi " + voter2);
+        }else if (vote1 == 3 ){
+            const push3 = harish.push(1);
+            const voter3 = harish.length;
+            console.log("Total vote for Divya " + voter3);
+        }
+        else if (vote1 == 4){
+            const push4 = akshaya.push(1);
+            const voter4 = akshaya.length;
+            console.log("Total votes for Aruna "+voter4);
+        } else{
+            console.log("enter the valid vote");
+        }
+
+
+    })
+    
 })
 app.get('/thank', function (req, res) {
     const htmlfilepath = path.join(__dirname, 'views', 'pages/final.html')
-    res.sendfile(htmlfilepath)
-    //res.render('confirm');
+    res.sendFile(htmlfilepath)
+    
+
 })
 //block convering
 class Block {
@@ -72,4 +111,4 @@ voting.addBlock(new Block(2, "28/11/2022", { amount: 1 }))
 console.log(JSON.stringify(voting, null, 4));
 
 //app lisean
-app.listen(3000);
+app.listen(4000);
