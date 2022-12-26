859e25d9105c5bbcf266040d8cba975e4c3a92d1
Harishkumar <harishkumarsp023@gmail.com>
2022-12-26 08:59:22 +0530

    the file is conveted to ejs

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const uuid = require('uuid');
 
 const express = require('express');
 const app = express();
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 //static file serving
 
 app.use(express.static('public'));
@@ -19,55 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 // end of serving
 //html
 app.get('/', function (req, res) {
-    const htmlfilepath = path.join(__dirname, 'views', 'index.html')
-    res.sendFile(htmlfilepath)
-    //res.render('confirm');
+
+    res.render('index');
 })
-app.get('/voter', function (req, res) {
-  
-    const htmlfilepath = path.join(__dirname, 'views', 'pages/voting.html')
-    res.sendFile(htmlfilepath)
-    //res.render('confirm');
-    app.post("/voter", function (req,res){
-        const divya = [1,1];
-        const karthi =[1,1];
-        const harish =[];
-        const akshaya =[];
-        const vote1 = req.body.nominee;
-        const name = req.body.name;
-        const voterid = req.body.voterid;
-        const composed = "voterName : "+ name + ", voter ID: " + voterid + ", Voting nominee : " + vote1;
-        console.log(composed);
-
-        if (vote1 == 1){
-            const push1 = divya.push(1);
-            const voter1 = divya.length;
-            console.log("total votes for Akshaya " + voter1);
-        }else if(vote1 == 2){
-            const push2 = karthi.push(1);
-            const voter2 = karthi.length;
-            console.log("Total vote for Karthi " + voter2);
-        }else if (vote1 == 3 ){
-            const push3 = harish.push(1);
-            const voter3 = harish.length;
-            console.log("Total vote for Divya " + voter3);
-        }
-        else if (vote1 == 4){
-            const push4 = akshaya.push(1);
-            const voter4 = akshaya.length;
-            console.log("Total votes for Aruna "+voter4);
-        } else{
-            console.log("enter the valid vote");
-        }
 
 
-    })
-    
-})
 app.get('/thank', function (req, res) {
-    const htmlfilepath = path.join(__dirname, 'views', 'pages/final.html')
-    res.sendFile(htmlfilepath)
-    
+    //const htmlfilepath = path.join(__dirname, 'views', 'pages/final.html')
+    //res.sendFile(htmlfilepath)
+    const filepath = path.join(__dirname, 'data', 'vote.json')
+    const filedata = fs.readFileSync(filepath)
+    const stored = JSON.parse(filedata);
+    res.render('pages/final', { vote: stored.length })
+
 
 })
 //block convering
@@ -110,5 +74,64 @@ voting.addBlock(new Block(2, "28/11/2022", { amount: 1 }))
 
 console.log(JSON.stringify(voting, null, 4));
 
+///form control
+app.get('/voter', function (req, res) {
+    //const htmlfilepath = path.join(__dirname, 'views', 'pages/voting.html')
+    //res.sendFile(htmlfilepath)
+    res.render('pages/voting');
+})
+app.post("/voter", function (req, res) {
+    const vote = req.body;
+    const filepath = path.join(__dirname, 'data', 'vote.json')
+    const filedata = fs.readFileSync(filepath)
+    const stored = JSON.parse(filedata);
+    stored.push(vote);
+    fs.writeFileSync(filepath, JSON.stringify(stored))
+
+    res.redirect('/thank')
+    console.log(vote)
+    // const divya = [1, 1];
+    // const karthi = [1, 1];
+    // const harish = [];
+    // const akshaya = [];
+    // const vote1 = req.body.nominee;
+    // const name = req.body.name;
+    // const voterid = req.body.voterid;
+    // const composed = "voterName : " + name + ", voter ID: " + voterid + ", Voting nominee : " + vote1;
+    // console.log(composed);
+
+    // if (vote1 == 1) {
+    //     const push1 = divya.push(1);
+    //     const voter1 = divya.length;
+    //     console.log("total votes for Akshaya " + voter1);
+    // } else if (vote1 == 2) {
+    //     const push2 = karthi.push(1);
+    //     const voter2 = karthi.length;
+    //     console.log("Total vote for Karthi " + voter2);
+    // } else if (vote1 == 3) {
+    //     const push3 = harish.push(1);
+    //     const voter3 = harish.length;
+    //     console.log("Total vote for Divya " + voter3);
+    // }
+    // else if (vote1 == 4) {
+    //     const push4 = akshaya.push(1);
+    //     const voter4 = akshaya.length;
+    //     console.log("Total votes for Aruna " + voter4);
+    // } else {
+    //     console.log("enter the valid vote");
+    // }
+
+
+})
+app.get('/result', function (req, res) {
+
+    const filepath = path.join(__dirname, 'data', 'vote.json')
+    const filedata = fs.readFileSync(filepath)
+    const stored = JSON.parse(filedata);
+    res.render('result', { vote: stored })
+
+
+})
+
 //app lisean
-app.listen(4000);
+app.listen(3000);
